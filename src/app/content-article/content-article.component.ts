0b666f864227b73b75ee55c95517c68a72943ff0
HDONG <377749229@qq.com>
2021-08-22 23:13:17 +0800

    完成了添加文章

--- src/app/content-article/content-article.component.ts
+++ src/app/content-article/content-article.component.ts
@@ -1,4 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuroraService } from '../aurora.service';
+
+interface httpMessage{
+  msg: string;
+  response:article[];
+  status: number;
+  success: boolean;
+}
+interface article {
+  content: string
+  cover: string
+  createTime: string
+  createUserId: number
+  headPortrait: string
+  id: number
+  tag: string
+  title: string
+  userName: string}
 
 @Component({
   selector: 'app-content-article',
@@ -6,10 +25,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-article.component.less']
 })
 export class ContentArticleComponent implements OnInit {
+  articleList:article[]=[];
 
-  constructor() { }
+  constructor(
+    public auroraService:AuroraService,
+    private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.getArticle ()
   }
+
+  getArticle () {
+    this.http.get<httpMessage> (this.auroraService.getArticleUrl)
+    .subscribe((res:httpMessage)=>{
+      if(res.success) {
+         this.articleList=res.response
+      }
+    })
+  }
+
 
 }
